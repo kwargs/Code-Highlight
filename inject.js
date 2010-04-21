6be5c2ccab877b1b8bde1130c56c8dfe393727bd
Alexander K <kwargs@gmail.com>
2010-04-21 09:48:18 +0400

    костыль для ярушки - замедденная инциализация

--- inject.js
+++ inject.js
@@ -1,23 +1,34 @@
-var __init__ = function(){
+var main = function(){
     var scripts = document.getElementsByTagName("script");
     for (var idx in scripts) {
         var s = scripts[idx];
         if (s && s.src && /(.*\/)highlight(\.pack)?.js/.test(s.src)) {
-            return
+            return;
         }
     }
 
-    // inject css
-    chrome.extension.sendRequest({ask: "favorite_style"}, function(response) {
-        var css_path = chrome.extension.getURL("styles/"+response.favorite_style+".css");
-        var css = document.createElement("link");
-        css.setAttribute("rel", "stylesheet");
-        css.setAttribute("type", "text/css");
-        css.setAttribute("href", css_path);
-        document.getElementsByTagName("head")[0].appendChild(css);
-    });
+    init_highlight = function(){
+        chrome.extension.sendRequest({ask: "favorite_style"}, function(response) {
+            var css_path = chrome.extension.getURL("styles/"+response.favorite_style+".css");
+            var css = document.createElement("link");
+            css.setAttribute("rel", "stylesheet");
+            css.setAttribute("type", "text/css");
+            css.setAttribute("href", css_path);
+            document.getElementsByTagName("head")[0].appendChild(css);
+        });
 
-    hljs.tabReplace = '    ';
-    hljs.initHighlighting();
+        hljs.tabReplace = '    ';
+        hljs.initHighlighting();
+    }
+    chrome.extension.sendRequest({ask: "page_settings"}, function(response) {
+        if (response.no_highlight){
+            return;
+        }
+        if (response.defered_highlight){
+            window.setTimeout(init_highlight, 1000);
+        } else {
+            init_highlight();
+        }
+    });
 };
-__init__();
+main();
